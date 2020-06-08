47cd01bc8ad6fe4180abb00eb9e326e81f07f72b
alexkimeu0 <kim3ual3x@gmail.com>
2020-06-08 14:48:20 +0300

    worked on Post & Posts components

---
+++ src/components/Post/Post.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+import "./Post.css";
+
+const Post = (props) => {
+  return (
+    <div className="post-container">
+      <div className="single-post">
+        <h1 className="post-heading">{props.post.title}</h1>
+        <img src={props.post.image} className="image" alt="post" />
+        <p>{props.post.body}</p>
+        <div className="info">
+          <h5>Article No: {props.id + 1}</h5>
+          <h4>Author: {props.post.author}</h4>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Post;
